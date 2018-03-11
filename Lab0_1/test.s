96a709c5479647394430d7ec375574c1cdc618a6
Grzegorz Wolanski <grzegorz.wolanski4321@wp.pl>
2018-03-12 00:57:42 +0100

    Adding new directory to remote branch

---
+++ Lab0_1/test.s
@@ -0,0 +1,8 @@
+#This file is containing simple read-write program
+#in assembler language. Program is written on x64
+#architecture and propably is going to expand for
+#extra functionalities.
+
+#Just a new blank comment.
+
+#And another one.
