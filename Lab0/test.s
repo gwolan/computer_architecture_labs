72876212ef856c5e5631f1416f05ddcfc566d89e
Grzegorz Wolanski <grzegorz.wolanski4321@wp.pl>
2018-03-14 01:57:47 +0100

    Added Makefile and extended test.s to simple input-output program

--- Lab0/test.s
+++ Lab0/test.s
@@ -6,3 +6,45 @@
 #Just a new blank comment.
 
 #And another one.
+
+EXIT    	 = 1
+READ    	 = 3
+WRITE   	 = 4
+STDIN   	 = 0
+STDOUT  	 = 1
+SYSCALL 	 = 0x80
+EXIT_SUCCESS = 0
+BUFFER       = 1
+
+.data
+	char: .byte 0x00
+
+
+.globl _start
+
+
+_start:
+movl $READ,   %eax
+movl $STDIN,  %ebx
+movl $char,   %ecx
+movl $BUFFER, %edx
+int  $SYSCALL
+
+
+cmpl $0, %eax
+je end
+
+
+movl $WRITE,  %eax
+movl $STDOUT, %ebx
+movl $char,   %ecx
+movl $BUFFER, %edx
+int  $SYSCALL
+
+jmp _start
+
+
+end:
+movl $EXIT, 	    %eax
+movl $EXIT_SUCCESS, %ebx
+int  $SYSCALL
